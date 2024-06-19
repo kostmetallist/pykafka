609a1290dcf4bdf56b8f52614a66087d095f5724
unknown <kostmetallist@gmail.com>
2024-06-19 15:38:25 +0300

    Add producer

---
+++ producer.py
@@ -0,0 +1,32 @@
+import json
+import os
+import random
+import time
+
+from kafka import KafkaProducer
+
+KAFKA_BOOTSTRAP_SERVERS = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 'localhost:29092')
+KAFKA_TOPIC_TEST = os.environ.get('KAFKA_TOPIC_TEST', 'test')
+# The version can be retrieved from within the kafka container:
+# $ docker exec kafka kafka-topics --version
+KAFKA_API_VERSION = os.environ.get('KAFKA_API_VERSION', '7.6.1')
+MESSAGES_TO_SEND = 30
+
+
+if __name__ == '__main__':
+
+    producer = KafkaProducer(
+        bootstrap_servers=[KAFKA_BOOTSTRAP_SERVERS],
+        api_version=KAFKA_API_VERSION
+    )
+
+    for i in range(MESSAGES_TO_SEND):
+        producer.send(
+            KAFKA_TOPIC_TEST,
+            json.dumps({
+                'body': f'Message #{i}'
+            }).encode('utf-8')
+        )
+        time.sleep(random(1, 5))
+
+    producer.flush()
